d26e1375152690a643217196b23dee00384ba767
SarahAllison92 <109143210+SarahAllison92@users.noreply.github.com>
2022-10-27 10:55:00 +0100

    Added the GitHub repository URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        <a
+          href="https://github.com/SarahAllison92/react-weather-app-project"
+          target="_blank"
+        >
+          Open-source code
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
