32119bf66fa0cfad7e4a8d9721082c3d03a7cc69
SarahAllison92 <109143210+SarahAllison92@users.noreply.github.com>
2022-10-27 11:18:58 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        <a
-          href="https://github.com/SarahAllison92/react-weather-app-project"
-          target="_blank"
-        >
-          Open-source code
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          <a
+            href="https://github.com/SarahAllison92/react-weather-app-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
