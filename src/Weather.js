8d8a0c5214ca21a95a9f132a8275813955e81441
SarahAllison92 <109143210+SarahAllison92@users.noreply.github.com>
2022-11-03 13:03:45 +0000

    Got search engine working

--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import "./Weather.css";
-import earthImage from "./earth.png";
 import axios from "axios";
-import FormattedDate from "./FormattedDate";
+import WeatherInfo from "./WeatherInfo";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
 
   function handleResponse(response) {
     setWeatherData({
@@ -20,70 +20,40 @@ export default function Weather(props) {
     });
   }
 
+  function search() {
+    const apiKey = "b6fdbb544a5ad3ta9845e350ff6o0437";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityInput(event) {
+    setCity(event.target.value);
+  }
+
   if (weatherData.ready) {
     return (
       <div className="Weather">
-        <form className="search-engine">
+        <form onSubmit={handleSubmit} className="search-engine">
           <input
             type="search"
             placeholder="Enter a City..."
             className="search-bar"
+            autoFocus="on"
+            onChange={handleCityInput}
           />
-
           <input type="submit" value="Search" className="button" />
         </form>
-        <div className="row">
-          <img src={earthImage} alt="earth" className="earth-image"></img>
-          <div className="col-6">
-            <div className="city">
-              <h1>{weatherData.city}</h1>
-              <h2 className="text-capitalize">
-                {weatherData.currentConditions}
-              </h2>
-              <div className="date">
-                Last updated:
-                <FormattedDate date={weatherData.date} />
-              </div>
-            </div>
-            <img
-              src={weatherData.icon}
-              width="80px"
-              alt={weatherData.currentConditions}
-              className="current-weather-icon"
-            />
-          </div>
 
-          <div className="col-6">
-            <ul className="current-conditions">
-              <li className="current-temp">
-                {Math.round(weatherData.temperature)}°C
-              </li>
-              <li className="current-humidity">
-                Humidity: {weatherData.humidity}%
-              </li>
-              <li className="current-wind-speed">
-                Wind: {Math.round(weatherData.wind)} km/h
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        <div className="weekly-forecast">
-          <div className="row">
-            <div className="col">Monday</div>
-            <div className="col">Tuesday</div>
-            <div className="col">Wednesday</div>
-            <div className="col">Thursday</div>
-            <div className="col">Friday</div>
-          </div>
-        </div>
+        <WeatherInfo data={weatherData} />
       </div>
     );
   } else {
-    const apiKey = "b6fdbb544a5ad3ta9845e350ff6o0437";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}`;
-    axios.get(apiUrl).then(handleResponse);
-
+    search();
     return "Loading....";
   }
 }
